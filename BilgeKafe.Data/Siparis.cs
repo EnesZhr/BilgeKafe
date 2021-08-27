447e5b2ec00c70505dc40513c3993a26dc115bb9
Enes ZEHİR <enes.zehir@hotmail.com>
2021-08-27 17:40:53 +0300

    Class Tanımlandı

---
+++ BilgeKafe.Data/Siparis.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class Siparis
+    {
+        public int MasaNo { get; set; }
+        public int SiparisDurum { get; set; }
+        public decimal OdenecekTutar { get; set; }
+        public DateTime? AcilisZamani { get; set; } = DateTime.Now;
+        public DateTime? KapanisZamani { get; set; }
+        public List<SiparisDetay> SiparisDetaylar { get; set; }
+        public string ToplamTutarTL { get; }
+
+        public decimal ToplamTutar()
+        {
+            decimal toplam = 0;
+            foreach (SiparisDetay detay in SiparisDetaylar)
+            {
+                toplam += detay.Tutar();
+            }
+            return toplam;
+        }
+
+
+
+        //* MasaNo: int
+        //* Durum: SiparisDurum
+        //* OdenenTutar: decimal
+        //* AcilisZamani: DateTime?
+        //* KapanisZamani: DateTime?
+        //* SiparisDetaylar: List<SiparisDetay>
+        //* ToplamTutarTL: string-readonly
+        //- ToplamTutar() : decimal
+
+    }
+}
---
+++ BilgeKafe.Data/SiparisDetay.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilgeKafe.Data
+{
+    public class SiparisDetay
+    {
+        public string UrunAd { get; set; }
+        public decimal BirimFiyat { get; set; }
+        public int Adet { get; set; }
+        public string TutarTL { get { return $"{Tutar():n2}₺"; } }
+
+        public  decimal Tutar()
+        {
+            return Adet * BirimFiyat;
+        }
+
+
+
+
+
+
+        //* UrunAd: string
+        //* BirimFiyat: decimal
+        //* Adet: int
+        //* TutarTL: string-readonly
+        //- Tutar() : decimal
+    }
+}
